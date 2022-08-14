80658feebdd053cc701d6968a3c63326601f379b
Joel Singh <joelsingh788@gmail.com>
2022-08-14 11:55:24 -0400

    Make equal button functional

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let screen = document.querySelector("#screen");
 screen.textContent = 0;
 let numbers = Array.from(document.querySelectorAll(".number"));
 let operators = Array.from(document.querySelectorAll(".operator"));
+let equal = document.querySelector("#equal");
 
 numbers.forEach((e, i) =>
   e.addEventListener("click", (e) => {
@@ -26,3 +27,24 @@ operators.forEach((e, i) =>
     readyForNextNum = true;
   })
 );
+
+equal.addEventListener("click", (e) => {
+  const calculate = (a, b) => {
+    switch (currentOperator) {
+      case "add":
+        return a + b;
+      case "divide":
+        return a / b;
+      case "multiply":
+        return a * b;
+      case "subtract":
+        return a - b;
+      default:
+        return undefined;
+    }
+  };
+  currentNum = calculate(pastNum, currentNum);
+  pastNum = null;
+  currentOperator = null;
+  screen.textContent = currentNum;
+});
