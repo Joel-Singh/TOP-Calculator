9c68fa58e45980cd48eaac18c4a7623506409690
Joel Singh <joelsingh788@gmail.com>
2022-08-14 11:35:10 -0400

    Make operator buttons keep track of current operator

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+let currentOperator = null;
 let currentNum = 0;
 let pastNum = null;
 let screen = document.querySelector("#screen");
 screen.textContent = 0;
 let numbers = Array.from(document.querySelectorAll(".number"));
+let operators = Array.from(document.querySelectorAll(".operator"));
 
 numbers.forEach((e, i) =>
   e.addEventListener("click", (e) => {
     currentNum = parseInt(currentNum + e.target.id);
     screen.textContent = currentNum;
+  })
+);
+
+operators.forEach((e, i) =>
+  e.addEventListener("click", (e) => {
+    currentOperator = e.target.id;
   })
 );
